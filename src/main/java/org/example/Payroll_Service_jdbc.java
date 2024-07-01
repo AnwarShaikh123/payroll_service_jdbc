0cb5607bb3b6b8ef79f809f93c31a48fe455b944
Mohd Anwar Shaikh <mohdanwarshaikh9@gmail.com>
2024-07-01 12:40:02 +0530

    Welcome to jdbc

--- src/main/java/org/example/Payroll_Service_jdbc.java
+++ src/main/java/org/example/Payroll_Service_jdbc.java
@@ -6,45 +6,16 @@ public class Payroll_Service_jdbc {
     public static void main(String[] args) {
         String url="jdbc:mysql://localhost:3306/payroll_service";
         String username="root";
-        String password="******";
+        String password="Anwarshaikh@8652741234";
         Connection connection=null;
-        Statement statement=null;
-        ResultSet rs= null;
         try{
             connection=DriverManager.getConnection(url,username,password);
             System.out.println("Connection Successfull");
-            String query= " SELECT * FROM employee_payroll";
-            statement=connection.createStatement();
-            rs=statement.executeQuery(query);
-            while (rs.next()){
-                System.out.println("Empolyee ID: " + rs.getInt(1));
-                System.out.println("Employee Name: " + rs.getString(2));
-                System.out.println("Employee Gender: " + rs.getString(3));
-                System.out.println("Employee Salary: " + rs.getInt(4));
-                System.out.println("Start Date: " + rs.getDate(5));
-                System.out.println("Employee Phone number: "  +rs.getInt(6));
-                System.out.println("Address: " + rs.getString(7));
-                System.out.println("Department: " + rs.getString(8));
-                System.out.println(" Basic pay: " + rs.getInt(9));
-                System.out.println(" Deductions: " +rs.getInt(10));
-                System.out.println("Taxable_pay: " +rs.getInt(11));
-                System.out.println(" Income_tax: " + rs.getInt(12));
-                System.out.println("Net_pay: " +rs.getInt(13));
-            }
-            System.out.println("Read successful");
+        }
+        catch (Exception e){
+            System.out.println("Connection failed");
 
-        }
-        catch (SQLException e){
-            System.out.println(" Error Connection to Database" + e.getMessage());
-        }
-        finally{
-            try {
-                if (rs != null) rs.close();
-                if (statement != null) statement.close();
-                if (connection != null) connection.close();
-            } catch (SQLException e) {
-                System.out.println("Error closing resources: " + e.getMessage());
         }
     }
 }
-}
+
