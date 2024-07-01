d5711f63797fd4d7968cd13ee262fc783ef37b57
Mohd Anwar Shaikh <mohdanwarshaikh9@gmail.com>
2024-07-01 10:58:01 +0530

    welcome to jDBC

---
+++ src/main/java/org/example/Payroll_Service_jdbc.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.sql.*;
+
+public class Payroll_Service_jdbc {
+    public static void main(String[] args) {
+        String url="jdbc:mysql://localhost:3306/payroll_service";
+        String username="root";
+        String password="Anwarshaikh@8652741234";
+        Connection connection=null;
+        try{
+            connection=DriverManager.getConnection(url,username,password);
+            System.out.println("Connection Successfull");
+        }
+        catch (Exception e){
+            System.out.println("Connection failed");
+
+        }
+    }
+}
